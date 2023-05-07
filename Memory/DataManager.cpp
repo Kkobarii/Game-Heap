ed00dfcc795cc4440da85db81136feb58430951e
Barbora Kovalská <barca.kovalska@gmail.com>
2023-05-07 18:17:03 +0200

    Added functionality for caching steam games data

--- Memory/DataManager.cpp
+++ Memory/DataManager.cpp
@@ -8,25 +8,28 @@ void DataManager::save_games(std::unordered_map<std::string, int> games, std::st
 
     //file <<  std::chrono::system_clock::now().time_since_epoch().count();
 
-    json json(games);
+    nlohmann::ordered_json json;
+    json["last_updated"] = std::chrono::system_clock::now().time_since_epoch().count();
+    json["total_games"] = games.size();
+    json["games"] = games;
+
 
     file << json.dump(4);
 
     file.close();
 }
 
-std::unordered_map<std::string, int> DataManager::load_games(std::string path)
+std::unordered_map<std::string, int> DataManager::load_games(std::string path, int refresh_rate)
 {
-    // TODO: add logic to check if file is recent
-    //  if so, load from file
-    //  if not, load from steam api and save to file
+    std::cout << "Loading games" << std::endl;
 
     std::ifstream file(path);
     json json;
     std::unordered_map<std::string, int> games;
 
-    if (!file.is_open())
+    if (!file.is_open() || !file.good())
     {
+        std::cout << " file not found, loading from steam api" << std::endl;
         std::string response;
         SteamAPI::get_games(response);
         json = json::parse(response);
@@ -40,8 +43,29 @@ std::unordered_map<std::string, int> DataManager::load_games(std::string path)
     }
     else
     {
-        file >> json;
-        games = json.get<std::unordered_map<std::string, int>>();
+        json = json::parse(file);
+        auto time_since = (std::chrono::system_clock::now().time_since_epoch().count() - json["last_updated"].get<long long>()) * std::chrono::system_clock::period::num / std::chrono::system_clock::period::den;
+        std::cout << " time since last update: " << time_since << " seconds" << std::endl;
+
+        if (time_since > refresh_rate)
+        {
+            std::cout << " file too old, loading from steam api" << std::endl;
+            std::string response;
+            SteamAPI::get_games(response);
+            json = json::parse(response);
+
+            for (auto& element: json["applist"]["apps"])
+            {
+                games[element["name"]] = element["appid"];
+            }
+
+            save_games(games, path);
+        }
+        else
+        {
+            std::cout << " file recent, loading from file" << std::endl;
+            games = json["games"].get<std::unordered_map<std::string, int>>();
+        }
     }
 
     file.close();
@@ -71,7 +95,7 @@ void DataManager::save_library(std::vector<std::shared_ptr<Game>> games, std::st
         {
             first = false;
         }
-        file << JSONParser::parse_from_game(*game);
+        file << parse_game_to_json(*game);
     }
 
     file << "\n\t]\n}" << std::endl;
@@ -97,7 +121,7 @@ std::vector<std::shared_ptr<Game>> DataManager::load_library(std::string path)
         file >> json;
         for (auto& element : json["games"])
         {
-            Game g = JSONParser::parse_to_game(element.dump());
+            Game g = parse_json_to_game(element.dump());
             games.push_back(std::make_shared<Game>(g));
         }
     }
